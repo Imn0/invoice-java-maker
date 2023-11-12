11732faa4d3c3d304f871f755fd665408d7b8e18
Imn0 <kzysiowy@gmail.com>
2023-11-12 17:27:39 +0100

    added example sql

--- src/main/java/fak/tura/repository/IElementRepository.java
+++
@@ -1,9 +0,0 @@
-package fak.tura.repository;
-
-import fak.tura.models.Element;
-
-public interface IElementRepository {
-    long saveElement(Element element);
-
-    Element getElement(String elementID);
-}
